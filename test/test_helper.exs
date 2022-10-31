20f4bdb95ea19089175dc8c0df76b1cc2e1d68dd
Paulo Daniel Gonzalez <paulo.gonzalez@hginsights.com>
2022-10-31 14:35:00 -0600

    Add context module, test and adjust test_helper to use Mox

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,7 @@
 Vapor.load!([%Vapor.Provider.Dotenv{}])
 
 Mimic.copy(Joken.Signer)
+Mox.defmock(TelemetryDispatchBehaviourMock, for: Avalanche.Telemetry.TelemetryDispatchBehaviour)
+Application.put_env(:avalanche, :telemetry_dispatch_impl, TelemetryDispatchBehaviourMock)
 
 ExUnit.start(exclude: [:skip, :integration])

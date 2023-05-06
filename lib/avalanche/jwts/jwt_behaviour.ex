395ce7c4ba2fc81b55687597532d7d1abdd098ea
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2023-05-06 17:18:16 -0500

    Dialyzer gods

--- lib/avalanche/jwts/jwt_behaviour.ex
+++ lib/avalanche/jwts/jwt_behaviour.ex
@@ -1,7 +1,7 @@
 defmodule Avalanche.JWTs.JWTBehaviour do
   @callback create_signer(binary(), map()) :: map()
-  @callback generate_claims(map(), binary()) :: {:ok, map()} | {:error, atom() | Keyword.t()}
-  @callback peek_claims(map()) :: {:ok, binary()} | {:error, binary()}
-  @callback peek_header(map()) :: {:ok, binary()} | {:error, binary()}
+  @callback generate_claims(keyword(), binary()) :: {:ok, map()} | {:error, atom() | keyword()}
+  @callback peek_claims(binary()) :: {:ok, binary()} | {:error, atom() | keyword()}
+  @callback peek_header(binary()) :: {:ok, binary()} | {:error, atom() | keyword()}
   @callback sign(map(), map()) :: {:ok, binary()} | {:error, binary()}
 end
